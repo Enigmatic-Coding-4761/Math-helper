bd48cacc6d8c43c9dee91996ed9c8b931be92cd6
alex forrest <blazezombie27@gmail.com>
2024-03-15 23:05:41 +0000

    subntract feature

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <string>
 #include "operators.h"
 using namespace std;
 
 int main(int argc, char* argv[]){
+  string choices[2] = {"add","subtract"};
   string option (argv[1]);
-  if (option == "add"){
-    cout << add(argc,argv);
+  for ( int i = 0; i < argc; i++){
+    if (option != choices[i]){}
   }
+
+  
 }
--- operators.h
+++ operators.h
@@ -18,4 +18,18 @@ float add(int argc, char* argv[]){
 }
 
 
+float take_away(int argc,char* argv[]){
+  float conv_argv[argc];
+  for (int i = 2;i<argc;i++){
+    float tmp = std::stof(argv[i]);
+    conv_argv[i] = tmp;
+  }
+  float tmp = conv_argv[2] - conv_argv[3];
+  for (int i = 4;i < argc;i++){
+    tmp = tmp + conv_argv[4];
+  }
+  return tmp;
+}
+
+
 #endif
